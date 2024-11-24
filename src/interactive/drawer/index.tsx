07ee8c3439f5a53894c2fb5b30cb1e5f383edfb2
AGLOP-1354 <jake@jober.io>
2024-11-24 22:09:23 +0900

    [drawer] 드로어 디자인 시스템 추가 [기타 작업] - framer motion 추가

---
+++ src/interactive/drawer/index.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, ReactNode } from "react";
+import { motion } from "framer-motion";
+
+import classes from "./drawer.module.css";
+
+const drawerVariants = {
+  open: {
+    y: 0,
+    transition: {
+      type: "tween",
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+  closed: {
+    y: "200%",
+    transition: {
+      type: "tween",
+      duration: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+type Props = {
+  open: boolean;
+  onClose?: () => void;
+  title?: string;
+  children: ReactNode;
+};
+
+const Drawer = ({ open, title, children }: Props) => {
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [open]);
+
+  return (
+    <motion.div
+      className={classes.Drawer}
+      variants={drawerVariants}
+      initial="closed"
+      animate={open ? "open" : "closed"}
+    >
+      {!!title && (
+        <header className={classes.header}>
+          <span className={classes.title}>{title}</span>
+          닫기버튼
+        </header>
+      )}
+
+      <article className={classes.article}>{children}</article>
+    </motion.div>
+  );
+};
+
+export default Drawer;
