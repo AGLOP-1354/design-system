ea59e48ab0e76d95bcc0a9827a908977b8150281
AGLOP-1354 <jake@jober.io>
2024-11-24 22:10:39 +0900

    [input] 인풋 디자인 시스템 추가

---
+++ src/interactive/input/index.tsx
@@ -0,0 +1,21 @@
+import React, { ChangeEvent } from "react";
+
+import classes from "./input.module.css";
+
+type Props = React.InputHTMLAttributes<HTMLInputElement> & {
+  value?: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  prefix?: string | React.ReactNode;
+  placeholder?: string;
+};
+
+const Input = ({ value, onChange, placeholder, prefix, ...acc }: Props) => {
+  return (
+    <div className={classes.inputContainer}>
+      {!!prefix && <span className={classes.prefix}>{prefix}</span>}
+      <input value={value} onChange={onChange} placeholder={placeholder} className={classes.Input} {...acc} />
+    </div>
+  );
+};
+
+export default Input;
