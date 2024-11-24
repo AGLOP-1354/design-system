5688a514571d58387f2730fa310ee27d0e9a47b8
AGLOP-1354 <jake@jober.io>
2024-11-24 23:21:51 +0900

    [공통] npmignore 설정

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import './global.css';
 
-import * as display from './display';
-import * as interactive from './interactive';
-
-export { display, interactive };
+export * from './display';
+export * from './interactive';
